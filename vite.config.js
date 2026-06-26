e510e7d15126c14f7fa919aaceaeff5842955cea
Patryk Domaszek <patryk.domaszek41@gmail.com>
2026-06-26 09:32:09 +0200

    Remove GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
 
-export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/HejMistrzu-Beta/" : "/",
+export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
