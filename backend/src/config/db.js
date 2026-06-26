186a601295b9998660c6c4d89ccf004e03501aa5
Patryk Domaszek <patryk.domaszek41@gmail.com>
2026-06-26 10:28:22 +0200

    Debug database config

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,13 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
+console.log("NODE_ENV:", process.env.NODE_ENV);
+
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  ssl: process.env.NODE_ENV === "production"
+  connectionString: process.env.DATABASE_URL,
+  ssl:
+  process.env.NODE_ENV === "production"
   ? { rejectUnauthorized: false }
   : false,
 });
