d951ed19d39710a6f9835710342b958945126a39
Patryk Domaszek <patryk.domaszek41@gmail.com>
2026-06-26 10:30:39 +0200

    Debug database config

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,9 +1,6 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
-console.log("NODE_ENV:", process.env.NODE_ENV);
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl:
