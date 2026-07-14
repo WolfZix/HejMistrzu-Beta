28bb34843b985b4001f4099d8cd0c5604e0f5190
Patryk Domaszek <patryk.domaszek41@gmail.com>
2026-07-14 15:19:51 +0200

    Updates

---
+++ assets/HistoryPage-B1_wvbft.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DbR80N_O.js";function e(){return t.jsx(t.Fragment,{})}export{e as default};
---
+++ assets/index-WQ0BBVcu.js
@@ -0,0 +1,1 @@
+function r(e){return e.toLowerCase().trim().normalize("NFD").replace(/[\u0300-\u036f]/g,"").replace(/\s+/g,"")}export{r as n};
