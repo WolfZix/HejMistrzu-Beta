9299b3d0278f4053828745a1f21d322c5adb6d7f
Patryk Domaszek <patryk.domaszek41@gmail.com>
2026-09-17 14:02:55 +0200

    fix button css

--- src/data/order.ts
+++ src/data/order.ts
@@ -6,4 +6,4 @@ export const optionClasses = "min-h-12 sm:min-h-16 rounded-xl border border-bord
 
 export const pickedOptionClasses = "ring-2 ring-primary bg-primary/10 text-primary";
 
-export const buttonClasses = "h-11 text-sm rounded-lg text-primary-foreground focus:ring-2 px-2 transition-all duration-200";
+export const buttonClasses = "h-11 text-sm rounded-lg text-primary-foreground focus:ring-2 px-2 transition-all duration-200 flex items-center justify-center gap-2";
