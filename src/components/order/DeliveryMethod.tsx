ea8a22415ed07d306424c6997c11d12650f7c866
Patryk Domaszek <patryk.domaszek41@gmail.com>
2026-09-17 13:34:41 +0200

    add portal to render widget on document.body

--- src/components/order/DeliveryMethod.tsx
+++ src/components/order/DeliveryMethod.tsx
@@ -3,6 +3,7 @@ import { Package, Truck, User } from "lucide-react";
 import { ValidationErrorsType, DeliveryMethodType, OrderData } from "@/types/order";
 import { useState } from "react";
 import InPostWidget from "@/components/InPostWidget";
+import { createPortal } from "react-dom";
 
 type DeliveryMethodProps = {
   deliveryMethod: DeliveryMethodType;
@@ -146,19 +147,21 @@ export default function DeliveryMethod({ deliveryMethod, formData, setFormData, 
           </div>
         </div>
       </div>
-      {isInPostOpen && (
-        <div className="fixed inset-0 z-[100] flex items-center justify-center bg-black/60 backdrop-blur-sm p-4">
-          <div className="relative w-full max-w-6xl h-[90vh] rounded-2xl bg-background overflow-hidden shadow-2xl">
-            <button
-              onClick={() => setIsInPostOpen(false)}
-              className="absolute right-4 top-4 z-20 flex h-10 w-10 items-center justify-center rounded-full bg-background/90 text-2xl text-foreground shadow-md transition hover:bg-primary hover:text-primary-foreground"
-            >
-              x
-            </button>
-            <InPostWidget />
-          </div>
-        </div>
-      )}
+      {isInPostOpen &&
+        createPortal(
+          <div className="fixed inset-0 z-[100] flex items-center justify-center bg-black/60 backdrop-blur-sm p-4">
+            <div className="relative w-full max-w-6xl h-[90vh] rounded-2xl bg-background overflow-hidden shadow-2xl">
+              <button
+                onClick={() => setIsInPostOpen(false)}
+                className="absolute right-4 top-4 z-20 flex h-10 w-10 items-center justify-center rounded-full bg-background/90 text-2xl text-foreground shadow-md transition hover:bg-primary hover:text-primary-foreground"
+              >
+                x
+              </button>
+              <InPostWidget />
+            </div>
+          </div>,
+          document.body
+        )}
     </>
   )
 }
